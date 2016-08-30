d154dbff895e3f91e96f65543abe7421d625d237
Felicity Tarnell <ft@le-Fay.ORG>
2016-08-30 17:01:14 +0100

    init.sh: fix ordering

--- init.sh
+++ init.sh
@@ -1,7 +1,8 @@
 #! /bin/sh
 
-/remap.pl >/usr/local/etc/trafficserver/remap.config
 CLUSTER_DNS_SUFFIX=$(sed -ne '/^search / { s/^search [a-zA-Z0-9-]*\.\([^ ]*\) .*$/\1/; p }' </etc/resolv.conf)
 export CLUSTER_DNS_SUFFIX
+
+/remap.pl >/usr/local/etc/trafficserver/remap.config
 
 exec /usr/local/bin/traffic_cop -o
