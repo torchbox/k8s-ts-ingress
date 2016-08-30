fd059630aa7982bdf70cb38df972f70ad04f24d4
Felicity Tarnell <ft@le-Fay.ORG>
2016-08-30 16:58:36 +0100

    use cluster dns suffix in remap

--- init.sh
+++ init.sh
@@ -1,3 +1,7 @@
 #! /bin/sh
 
 /remap.pl >/usr/local/etc/trafficserver/remap.config
+CLUSTER_DNS_SUFFIX=$(sed -ne '/^search / { s/^search [a-zA-Z0-9-]*\.\([^ ]*\) .*$/\1/; p }' </etc/resolv.conf)
+export CLUSTER_DNS_SUFFIX
+
+exec /usr/local/bin/traffic_cop -o
