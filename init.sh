7720fe96925844a6ac34ad634a677484ac9dfbf1
Felicity Tarnell <ft@le-Fay.ORG>
2016-08-30 17:30:22 +0100

    init.sh: poll for resource changes

--- init.sh
+++ init.sh
@@ -5,4 +5,16 @@ export CLUSTER_DNS_SUFFIX
 
 /remap.pl >/usr/local/etc/trafficserver/remap.config
 
+(
+	while true; do
+		sleep 30
+
+		/remap.pl >/usr/local/etc/trafficserver/remap.config.tmp
+		if ! cmp /usr/local/etc/trafficserver/remap.config.tmp /usr/local/etc/trafficserver/remap.config; then
+			mv /usr/local/etc/trafficserver/remap.config.tmp /usr/local/etc/trafficserver/remap.config
+			/usr/local/bin/traffic_ctl config reload
+		fi
+	done
+) &
+
 exec /usr/local/bin/traffic_cop -o
