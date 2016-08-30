52f012ccc6fea560f5ef3707460de046df023d5c
Felicity Tarnell <ft@le-Fay.ORG>
2016-08-30 17:12:27 +0100

    Revert "use search_default_domains"
    This reverts commit 0df4ede9d8f87e16a8b16fb14c962ec6adc34d3e.

--- init.sh
+++ init.sh
@@ -1,5 +1,8 @@
 #! /bin/sh
 
+CLUSTER_DNS_SUFFIX=$(sed -ne '/^search / { s/^search [a-zA-Z0-9-]*\.\([^ ]*\) .*$/\1/; p }' </etc/resolv.conf)
+export CLUSTER_DNS_SUFFIX
+
 /remap.pl >/usr/local/etc/trafficserver/remap.config
 
 exec /usr/local/bin/traffic_cop -o
