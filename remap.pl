2791692bf39aedbdf4eb13f606f6f54614664309
Felicity Tarnell <ft@le-Fay.ORG>
2016-09-01 15:26:05 +0100

    support deis routing

--- remap.pl
+++ remap.pl
@@ -16,17 +16,21 @@ my $apiroot;
 my $testmode = 0;
 my $ssldir = '/usr/local/etc/trafficserver/ssl';
 my $must_reload = 0;
+my $remap_file = '/usr/local/etc/trafficserver/remap.config';
+my $multicert_file = '/usr/local/etc/trafficserver/remap.config';
 
 if (@ARGV > 0 and $ARGV[0] eq '--test') {
   print STDERR "test mode\n";
-  $apiroot = $ARGV[1];
+  $apiroot = $ARGV[1] || 'https://master.kube.itl.rslon.torchbox.net:8443';
   $ua->ssl_opts(
-    SSL_cert_file => $ARGV[2],
-    SSL_key_file  => $ARGV[3],
-    SSL_ca_file   => $ARGV[4]);
+    SSL_cert_file => $ARGV[2] || '/home/ft/.kube/admin@kube.itl.rslon.torchbox.net.crt',
+    SSL_key_file  => $ARGV[3] || '/home/ft/.kube/admin@kube.itl.rslon.torchbox.net.key',
+    SSL_ca_file   => $ARGV[4] || '/etc/ssl/certs/tbx-ca.pem',
+  );
   $testmode = 1;
   $ssldir = './certs';
-  open REMAP, ">./remap.config";
+  $remap_file = './remap.config';
+  $multicert_file = './ssl_multicert.config';
 } else {
   my $cacertfile = "/var/run/secrets/kubernetes.io/serviceaccount/ca.crt";
   my $tokenfile = "/var/run/secrets/kubernetes.io/serviceaccount/token";
@@ -39,7 +43,6 @@ if (@ARGV > 0 and $ARGV[0] eq '--test') {
   $apiroot = "https://$ENV{KUBERNETES_SERVICE_HOST}:$ENV{KUBERNETES_PORT_443_TCP_PORT}";
   $ua->default_header("Authorization" => "Bearer $token");
   $ua->ssl_opts('SSL_ca_file' => $cacertfile);
-  open REMAP, ">/usr/local/etc/trafficserver/remap.config";
 }
 
 if (!-d $ssldir) {
@@ -54,6 +57,27 @@ my $ret = decode_json $nsresp->content;
 my @remaps;
 my @tls;
 
+sub check_file_changed {
+  my $file = shift;
+  my $new = shift;
+
+  if (!-f $file) {
+    return 1;
+  }
+
+  local $/ = undef;
+  open CUR, "<$file";
+  binmode CUR;
+  my $current = <CUR>;
+  close CUR;
+
+  if (md5_hex($current) ne md5_hex($new)) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
+
 sub get_service_ip {
   my $nsname = shift;
   my $service = shift;
@@ -61,6 +85,30 @@ sub get_service_ip {
   my $svcret = decode_json $svcresp->content;
 
   return $svcret->{'spec'}->{'clusterIP'};
+}
+
+sub handle_deis_service {
+  my $deisdomain = shift;
+  my $svc = shift;
+  my $svcname = $svc->{'metadata'}->{'name'};
+  my $svcns = $svc->{'metadata'}->{'namespace'};
+
+  print "handling deis service $svcns/$svcname\n";
+  my @domains = split(",", $svc->{'metadata'}->{'annotations'}->{'router.deis.io/domains'});
+  return unless @domains;
+
+  foreach my $port (@{$svc->{'spec'}->{'ports'}}) {
+    my $backport = $port->{'port'};
+
+    foreach my $domain (@domains) {
+      print "domain: $domain/$backport\n";
+      my $backip = get_service_ip($svcns, $svcname);
+      
+      my $service = "$backip:$backport";
+      $domain =~ s/$/.$deisdomain/ if $domain !~ /\./;
+      push @remaps, { host => $domain, prefix => '/', service => $service };
+    }
+  }
 }
 
 foreach my $namespace (@{$ret->{'items'}}) {
@@ -102,7 +150,28 @@ foreach my $namespace (@{$ret->{'items'}}) {
       }
     }
   }
+
+  my $svcsresp = $ua->get($apiroot . "/api/v1/namespaces/${nsname}/services/");
+  if ($svcsresp->is_success) {
+    my $svcsret = decode_json $svcsresp->content;
+
+    my $deisresp = $ua->get($apiroot . "/api/v1/namespaces/${nsname}/replicationcontrollers/deis-router");
+    if ($deisresp->is_success) {
+      my $deisret = decode_json $deisresp->content;
+      my $deisdomain = $deisret->{'metadata'}->{'annotations'}->{'router.deis.io/nginx.platformDomain'};
+
+      foreach my $service (@{$svcsret->{'items'}}) {
+        if (defined($service->{'metadata'}->{'labels'}->{'router.deis.io/routable'}) and
+            $service->{'metadata'}->{'labels'}->{'router.deis.io/routable'} eq 'true')
+        {
+          handle_deis_service($deisdomain, $service);
+        }
+      }
+    }
+  }
 }
+
+my $remap_content = "";
 
 foreach my $remap (sort { length($b->{'prefix'} || '/') <=> length($a->{'prefix'} || '/') } @remaps) {
   my $host = $remap->{'host'};
@@ -113,42 +182,42 @@ foreach my $remap (sort { length($b->{'prefix'} || '/') <=> length($a->{'prefix'
 		my $this_extra = $remap_extra;
 		$this_extra =~ s/<hostname>/$host/g;
 		$this_extra =~ s/<protocol>/http/g;
-		print REMAP "map http://$host$prefix http://$service$prefix $this_extra\n";
+		$remap_content .= "map http://$host$prefix http://$service$prefix $this_extra\n";
 
 		$this_extra = $remap_extra;
 		$this_extra =~ s/<hostname>/$host/g;
 		$this_extra =~ s/<protocol>/https/g;
-		print REMAP "map https://$host$prefix http://$service$prefix $this_extra\n";
+		$remap_content .= "map https://$host$prefix http://$service$prefix $this_extra\n";
 
 		$this_extra = $remap_extra;
 		$this_extra =~ s/<hostname>/$host/g;
 		$this_extra =~ s/<protocol>/ws/g;
-		print REMAP "map ws://$host$prefix ws://$service$prefix $this_extra\n";
+		$remap_content .= "map ws://$host$prefix ws://$service$prefix $this_extra\n";
 
 		$this_extra = $remap_extra;
 		$this_extra =~ s/<hostname>/$host/g;
 		$this_extra =~ s/<protocol>/wss/g;
-		print REMAP "regex_map wss://$host$prefix ws://$service$prefix $this_extra\n";
+		$remap_content .= "regex_map wss://$host$prefix ws://$service$prefix $this_extra\n";
 	} else {
 		my $this_extra = $remap_extra;
 		$this_extra =~ s/<hostname>/__ANY__/g;
 		$this_extra =~ s/<protocol>/http/g;
-		print REMAP "regex_map http://[A-Za-z0-9-]+$prefix http://$service$prefix $this_extra\n";
+		$remap_content .= "regex_map http://[A-Za-z0-9-]+$prefix http://$service$prefix $this_extra\n";
 
 		$this_extra = $remap_extra;
 		$this_extra =~ s/<hostname>/__ANY__/g;
 		$this_extra =~ s/<protocol>/https/g;
-		print REMAP "regex_map http://[A-Za-z0-9-]+$prefix https://$service$prefix $this_extra\n";
+		$remap_content .= "regex_map http://[A-Za-z0-9-]+$prefix https://$service$prefix $this_extra\n";
 
 		$this_extra = $remap_extra;
 		$this_extra =~ s/<hostname>/__ANY__/g;
 		$this_extra =~ s/<protocol>/ws/g;
-		print REMAP "regex_map ws://[A-Za-z0-9-]+$prefix ws://$service$prefix $this_extra\n";
+		$remap_content .= "regex_map ws://[A-Za-z0-9-]+$prefix ws://$service$prefix $this_extra\n";
 
 		$this_extra = $remap_extra;
 		$this_extra =~ s/<hostname>/__ANY__/g;
 		$this_extra =~ s/<protocol>/wss/g;
-		print REMAP "regex_map wss://[A-Za-z0-9-]+$prefix ws://$service$prefix $this_extra\n";
+		$remap_content .= "regex_map wss://[A-Za-z0-9-]+$prefix ws://$service$prefix $this_extra\n";
 	}
 }
 
@@ -159,7 +228,7 @@ foreach my $tls (@tls) {
   my $secret = $tls->{'secret'};
 
   my $secresp = $ua->get($apiroot . "/api/v1/namespaces/${nsname}/secrets/$secret");
-  next if $secresp->status_code == 404;
+  next unless $secresp->is_success;
   my $secrt = decode_json $secresp->content;
 
   my $certfile = $ssldir."/$nsname-$secret.crt";
@@ -194,23 +263,25 @@ foreach my $tls (@tls) {
   $must_reload = 1;
 }
 
-my $multicert_current;
-{
-  local $/ = undef;
-  open CUR, "</usr/local/etc/trafficserver/ssl_multicert.config";
-  binmode CUR;
-  $multicert_current = <CUR>;
-  close CUR;
-}
-
-if (md5_hex($multicert_current) ne md5_hex($multicert_new)) {
-  open MC, ">/usr/local/etc/trafficserver/ssl_multicert.config";
+if (check_file_changed($multicert_file, $multicert_new)) {
+  print "remap.pl: ssl_multicert.config has changed\n";
+  open MC, ">$multicert_file";
   binmode MC;
   print MC $multicert_new;
+  close MC;
+  $must_reload = 1;
+}
+
+if (check_file_changed($remap_file, $remap_content)) {
+  print "remap.pl: remap.config has changed\n";
+  open RM, ">$remap_file";
+  binmode RM;
+  print RM $remap_content;
+  close RM;
   $must_reload = 1;
 }
 
 if ($must_reload) {
-  print "remap.pl: reloading TS configuration.";
+  print "remap.pl: reloading TS configuration.\n";
   system("/usr/local/bin/traffic_ctl", "config", "reload");
 }
