621d4cf63cb2a33789cb2af6de1ddf2b2d88e801
Liss Tarnell <ft@le-Fay.ORG>
2017-06-29 10:25:13 +0100

    travis-build.sh: use folds instead of logfile

--- travis-build.sh
+++ travis-build.sh
@@ -9,34 +9,43 @@ else
 	VERSION=$COMMIT
 fi
 
+printf 'travis_fold:start:build-release\r'
 printf '####################################################################\n'
 printf '>>> Building release.\n\n'
 
 # Always build from a release, so we know the release process works.
 make -f Makefile.dist VERSION=$VERSION release
 
+printf 'travis_fold:end:build_release\r'
+
 cd k8s-ts-ingress-$VERSION
 
+printf 'travis_fold:start:build-docker\r'
 printf '####################################################################\n'
 printf '>>> Building Docker image.\n\n'
 # This tests the build and runs basic unit tests.
 DOCKER_REPOSITORY=torchbox/k8s-ts-ingress
-docker build --pull -t $DOCKER_REPOSITORY:$COMMIT . >log 2>&1 || (cat log; exit 1)
+docker build --pull -t $DOCKER_REPOSITORY:$COMMIT .
+printf 'travis_fold:end:build_docker\r'
 
+printf 'travis_fold:start:test-e2e\r'
 printf '####################################################################\n'
 printf '>>> Running end-to-end tests.\n\n'
 # Run e2e tests.
-sudo apt-get -qq update >log 2>&1 || (cat log; exit 1)
+sudo apt-get -qq update
 sudo apt-get -qy install libjson-c-dev libcurl4-openssl-dev libssl-dev \
-	     netcat-traditional pkg-config  >log 2>&1 || (cat log; exit 1)
+	     netcat-traditional pkg-config
 tests/e2erun.sh
+printf 'travis_fold:end:test-e2e\r'
 
 # If this is a release, push the Docker image to Docker Hub.
 if [ "$TRAVIS_PULL_REQUEST" = "false" -a -n "$TRAVIS_TAG" ]; then
+	printf 'travis_fold:start:release\r'
 	printf '####################################################################\n'
 	printf '>>> Creating release.\n\n'
 
 	docker login -u $DOCKER_USER -p $DOCKER_PASSWORD
 	docker tag $DOCKER_REPOSITORY:$COMMIT $DOCKER_REPOSITORY:$VERSION
 	docker push $DOCKER_REPOSITORY:$VERSION
+	printf 'travis_fold:end:release\r'
 fi
